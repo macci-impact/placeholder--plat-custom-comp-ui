0cb46d62092035140dafb76ea956981887a2918a
Macci Macaranas <macci.macaranas@impact.com>
2025-11-27 11:44:07 -0800

    prep

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { defineCustomElement } from "vue";
 
 import SignUpOrLogIn from "./components/accept-invitation/SignUpOrLogIn.vue";
-import TableFilterToggle from "./components/dynamic-partials/table-toggle/TableFilterToggle.vue";
-import TableToggle from "./components/dynamic-partials/table-toggle/TableToggle.vue";
-import HorizontalTabs from "./components/general/HorizontalTabs.vue";
-import ImagePreview from "./components/general/ImagePreview.vue";
-import LoginTwoFa from "./components/login-2fa/LoginTwoFa.vue";
-import RolesAndPermissions from "./components/invite-user/RolesAndPermissions.vue";
+// import TableFilterToggle from "./components/dynamic-partials/table-toggle/TableFilterToggle.vue";
+// import TableToggle from "./components/dynamic-partials/table-toggle/TableToggle.vue";
+// import HorizontalTabs from "./components/general/HorizontalTabs.vue";
+// import ImagePreview from "./components/general/ImagePreview.vue";
+// import LoginTwoFa from "./components/login-2fa/LoginTwoFa.vue";
+// import RolesAndPermissions from "./components/invite-user/RolesAndPermissions.vue";
 import LoadMask from "./components/general/LoadMask.vue";
-import PsaLanding from "./components/psa/PsaLanding.vue";
+// import PsaLanding from "./components/psa/PsaLanding.vue";
 import PreviewLinkUrl from "@/components/branding/PreviewLinkUrl.vue";
 
 const PreviewLinkUrlElement = defineCustomElement(PreviewLinkUrl);
@@ -18,16 +18,16 @@ if (!customElements.get('preview-link-url')) {
 }
 
 export {
-    HorizontalTabs,
-    ImagePreview,
-    LoadMask,
-    LoginTwoFa,
+    // HorizontalTabs,
+    // ImagePreview,
+    // LoadMask,
+    // LoginTwoFa,
     PreviewLinkUrl,
-    PsaLanding,
-    RolesAndPermissions,
+    // PsaLanding,
+    // RolesAndPermissions,
     SignUpOrLogIn,
-    TableToggle,
-    TableFilterToggle,
+    // TableToggle,
+    // TableFilterToggle,
 }
 
 (window as any)['platform-custom-components'] = {
